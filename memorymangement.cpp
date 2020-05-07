4a9747068eee18ddd91eb17711ce7e0dad6420be
Muhammad Kamal <muhammedkamal022@gmail.com>
2020-05-07 11:59:48 +0200

    editing display and undisplay inputs

--- memorymangement.cpp
+++ memorymangement.cpp
@@ -15,6 +15,22 @@ MemoryMangement::MemoryMangement(QWidget *parent)
     , ui(new Ui::MemoryMangement)
 {
     ui->setupUi(this);
+    ui->label_numberOfHoles->hide();
+    ui->spinBox_HolesCount->hide();
+    ui->label_numberOfProcess->hide();
+    ui->spinBox_ProcessesCount->hide();
+    ui->label_holesStart->hide();
+    ui->spinBox_holeStart->hide();
+    ui->label_HoleSize->hide();
+    ui->spinBox_holeSize->hide();
+    ui->label_segmentCount->hide();
+    ui->spinBox_segmentsCount->hide();
+    ui->label_segmentSize->hide();
+    ui->spinBox_segmentSize->hide();
+    ui->label_memoryFit->hide();
+    ui->radioButton_BestFit->hide();
+    ui->radioButton_FirstFit->hide();
+
 }
 
 MemoryMangement::~MemoryMangement()
@@ -40,6 +56,8 @@ void MemoryMangement::on_spinBox_memorySize_editingFinished()
             ui->listWidget_processes->item(var)->setBackgroundColor(Qt::gray);
         }
         assigned=true;
+        ui->label_numberOfHoles->show();
+        ui->spinBox_HolesCount->show();
     }
     if (assigned)
     {
@@ -65,7 +83,10 @@ void MemoryMangement::on_spinBox_HolesCount_editingFinished()
     ui->label_holesStart->setText("Enter 1 hole start adress");
     ui->label_HoleSize->setText("Enter 1 hole size");
     holesCount=1;
-
+    ui->label_holesStart->show();
+    ui->spinBox_holeStart->show();
+    ui->label_HoleSize->show();
+    ui->spinBox_holeSize->show();
 }
 
 void MemoryMangement::on_spinBox_holeStart_editingFinished()
@@ -85,8 +106,19 @@ void MemoryMangement::on_spinBox_holeSize_editingFinished()
            ui->listWidget_processes->item(i)->setForeground(Qt::white);
         }
         holesCount++;
+        if (holesCount<=holesTotalCount)
+        {
         ui->label_holesStart->setText("Enter "+QString::number(holesCount) +" hole start adress");
-        ui->label_HoleSize->setText("Enter "+QString::number(holesCount) +" hole size");
+        ui->label_HoleSize->setText("Enter "+QString::number(holesCount) +" hole size");}
+    }
+    if (holesCount>holesTotalCount)
+    {
+        ui->label_holesStart->hide();
+        ui->spinBox_holeStart->hide();
+        ui->label_HoleSize->hide();
+        ui->spinBox_holeSize->hide();
+        ui->label_numberOfProcess->show();
+        ui->spinBox_ProcessesCount->show();
     }
 
 }
@@ -119,7 +151,10 @@ void MemoryMangement::on_spinBox_ProcessesCount_editingFinished()
     processesTotalCount=ui->spinBox_ProcessesCount->value();
     ui->label_segmentCount->setText("Enter 1 process segments Count");
     processesCount=1;
-
+    ui->label_segmentCount->show();
+    ui->spinBox_segmentsCount->show();
+    ui->label_segmentSize->show();
+    ui->spinBox_segmentSize->show();
     //initialize vector of processes here
 }
 
@@ -148,5 +183,14 @@ void MemoryMangement::on_spinBox_segmentSize_editingFinished()
         ui->label_segmentSize->setText("Enter process "+QString::number(processesCount)+" segment "+QString::number(segmentsCount) +" segment size");
         ui->label_segmentCount->setText("Enter "+QString::number(processesCount) +" process segments Count");
         segmentTotalCount=ui->spinBox_segmentsCount->value();
+    }
+    else {
+        ui->label_segmentCount->hide();
+        ui->spinBox_segmentsCount->hide();
+        ui->label_segmentSize->hide();
+        ui->spinBox_segmentSize->hide();
+        ui->label_memoryFit->show();
+        ui->radioButton_BestFit->show();
+        ui->radioButton_FirstFit->show();
     }
 }
