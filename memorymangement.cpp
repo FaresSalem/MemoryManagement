b6640b9e2040a007c22fe6857ff5395bd1c1cb38
Muhammad Kamal <muhammedkamal022@gmail.com>
2020-05-09 19:08:52 +0200

    preproccesses added

--- memorymangement.cpp
+++ memorymangement.cpp
@@ -1,13 +1,17 @@
 #include "memorymangement.h"
 #include "ui_memorymangement.h"
 #include <QMessageBox>
-
+#include <iostream>
+#include <vector>
 // here is global variables to be shared with whole classes and functions
 QVector<int>memorySize(0);
 int holesCount,holesTotalCount, HoleStartAdress, holeSize,
-processesCount,processesTotalCount,
-segmentsCount,segmentTotalCount,
-segmentSize;
+processesCount=1,processesTotalCount,
+segmentsCount,segmentTotalCount,segmentSize;
+
+
+using namespace std;
+
 
 
 MemoryMangement::MemoryMangement(QWidget *parent)
@@ -32,6 +36,90 @@ MemoryMangement::MemoryMangement(QWidget *parent)
     ui->radioButton_FirstFit->hide();
 
 }
+typedef struct hole
+{
+    size_t base;
+    size_t limit;
+}Hole;
+
+typedef struct segment
+{
+    string segment_name;
+    size_t limit;
+}Segment;
+
+typedef struct process
+{
+    size_t process_id;
+    vector <Segment> seg_list;
+}Process;
+
+vector <hole> hole_list;                           //data structure to store information of holes in memory
+
+vector <Process> process_list;                     //data structure to store information of processes in memory
+vector <int> memory;
+void assign_best()
+{
+
+}
+void initialize_memory(size_t mem_size)
+{
+    for (size_t i = 0; i < mem_size;i++)
+    {
+        memory[i]=-1;//
+    }
+}
+// assigning holes additionally assigns 2 processes at maximum
+// if hole assigned to the start of the memory the next total memory will be assigned as a process
+// if hole assigned to the end of the memory, the previous total memory will be assigned as a process
+// if there is no holes before this hole, the memory the previous total memory will be assigned as a process
+// if there is no holes after this hole, the memory the previous total memory will be assigned as a process
+// if there is hole before or after this hole, assign process only to the opposite direction.
+
+
+void assign_hole(size_t base, size_t limit)
+{
+
+    hole_list.push_back({
+                       base,
+                       limit });
+    for (size_t j = base;j < base+limit;j++)
+            memory[j] = 0;
+    if (memory[base-1]==-1)
+    {
+        for (int i=base-1;i>=0&&memory[i]==-1;i--)
+        {
+            memory[i]=processesCount;
+        }
+        processesCount++;
+    }
+     if (memory[base+limit]==-1||memory[base+limit]>0)
+    {
+        for (size_t i=base+limit;i<memory.size()&&(memory[i]>0||memory[i]==-1);i++)
+          {
+            memory[i]=processesCount;
+          }
+        processesCount++;
+    }
+}
+void assign_process(size_t process_id, size_t num_seg)
+{
+    vector<Segment> seg_list;
+    size_t limit;
+    string segment_name;
+    for (size_t i = 0;i < num_seg;i++)
+    {
+        cin >> segment_name;
+        cin >> limit;
+        Segment s1 =
+        {
+            segment_name,
+            limit
+        };
+        seg_list.push_back(s1);
+    }
+    process_list.push_back({ process_id,seg_list });
+}
 
 MemoryMangement::~MemoryMangement()
 {
@@ -45,7 +133,9 @@ void MemoryMangement::on_spinBox_memorySize_editingFinished()
     if (!assigned)
     {
         int size=ui->spinBox_memorySize->value();
-        memorySize.resize(size); // resize memory size
+        memorySize.resize(size);// resize memory size
+        memory.resize(size);
+        initialize_memory(size);
         //QString text ="size="+QString::number(size);
         //QString text2 ="first="+QString::number(memorySize[0]);
         //QMessageBox::information(this,text,text2);
@@ -63,13 +153,13 @@ void MemoryMangement::on_spinBox_memorySize_editingFinished()
     {
         ui->listWidget_processes->clear();
         int size=ui->spinBox_memorySize->value();
-        memorySize.resize(size); // resize memory size
-        //QString text ="size="+QString::number(size);
-        //QString text2 ="first="+QString::number(memorySize[0]);
-        //QMessageBox::information(this,text,text2);
+        memorySize.resize(size);
+        memory.resize(size);
+        initialize_memory(size);
+
 
         for(int var=0;var<memorySize.size();var++) {
-            ui->listWidget_processes->addItem(QString::number(var));
+            ui->listWidget_processes->addItem("");
             ui->listWidget_processes->item(var)->setTextAlignment(Qt::AlignHCenter);
             ui->listWidget_processes->item(var)->setBackgroundColor(Qt::gray);
         }
@@ -98,8 +188,12 @@ void MemoryMangement::on_spinBox_holeSize_editingFinished()
 {
     if (holesCount<=holesTotalCount)
     {
+
         holeSize=ui->spinBox_holeSize->value();
+        assign_hole(HoleStartAdress,holeSize); // assigning hole function
         // here we would pass it to the the functin hole.assign
+        ui->statusbar->showMessage(QString::number(memory[HoleStartAdress-1]));
+
         for (int i=HoleStartAdress;i<HoleStartAdress+holeSize;i++) {
            ui->listWidget_processes->item(i)->setText("hole "+QString::number(holesCount));
            ui->listWidget_processes->item(i)->setBackgroundColor(Qt::black);
@@ -119,6 +213,14 @@ void MemoryMangement::on_spinBox_holeSize_editingFinished()
         ui->spinBox_holeSize->hide();
         ui->label_numberOfProcess->show();
         ui->spinBox_ProcessesCount->show();
+    }
+    for(size_t var=0;var<memory.size();var++) {
+        if (memory[var]!=-1 && memory[var]!=0)
+        {
+            ui->listWidget_processes->item(var)->setText("process "+QString::number(memory[var]));
+            ui->listWidget_processes->item(var)->setTextAlignment(Qt::AlignHCenter);
+            ui->listWidget_processes->item(var)->setBackgroundColor(Qt::blue);
+        }
     }
 
 }
